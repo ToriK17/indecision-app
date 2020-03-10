8e92f1a596ccbd2e2f8fe65edba4ebc0b6df69ca
ToriK17 <vfcad@protonmail.com>
2020-03-10 16:40:12 -0700

    adds babel config for app.js files

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,15 @@
+'use strict';
+
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
 
-// var template = <p> This is JSX from app.js </p>;
-var template = React.createElement("p", null, " This is JSX from app.js ");
+var template = React.createElement(
+  'p',
+  null,
+  ' This is JSX from app.js, Adding changes!!!! '
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
